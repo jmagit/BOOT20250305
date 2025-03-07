b23a25818234d1e50d2fa6f02aeb4fb9e71948c5
Javier <jmartin@grupoloyal.com>
2025-03-07 13:05:21 +0100

    @PostConstruct

--- demo/src/main/java/com/example/ioc/Registro.java
+++ demo/src/main/java/com/example/ioc/Registro.java
@@ -1,10 +1,21 @@
 package com.example.ioc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import jakarta.annotation.PostConstruct;
 
 @Component
 public class Registro {
+	@Autowired
+	private Configuracion config;
+	
 	public Registro() {
 		System.err.println("Registro creado");
+	}
+	
+	@PostConstruct
+	private void init() {
+		config.init();
 	}
 }
