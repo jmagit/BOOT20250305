7c7653a982e6531e6d526838ea9deed747688edd
Javier <jmartin@grupoloyal.com>
2025-03-07 12:26:29 +0100

    @Qualifier

--- demo/src/main/java/com/example/ioc/RepositorioImpl.java
+++ demo/src/main/java/com/example/ioc/RepositorioImpl.java
@@ -1,10 +1,12 @@
 package com.example.ioc;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@Qualifier("verdad")
 public class RepositorioImpl implements Repositorio {
-	public RepositorioImpl(Configuracion config) {
+	public RepositorioImpl(Configuracion config, Registro registro) {
 		// TODO Auto-generated constructor stub
 	}
 	@Override
