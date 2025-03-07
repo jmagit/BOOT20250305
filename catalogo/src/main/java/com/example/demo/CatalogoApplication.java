a9f85095a544b2b4e95baa339a7256666b67066b
Javier <jmartin@grupoloyal.com>
2025-03-07 08:54:46 +0100

    creacion del proyecto catalogo

--- catalogo/src/main/java/com/example/demo/CatalogoApplication.java
+++ catalogo/src/main/java/com/example/demo/CatalogoApplication.java
@@ -1,28 +1,19 @@
-package com.example;
+package com.example.demo;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class DemoApplication implements CommandLineRunner {
+public class CatalogoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		
+		SpringApplication.run(CatalogoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
 		System.err.println("Aplicacion arrancada");
-	}
-	
-	@Bean
-  	CommandLineRunner demo() {
-		return (args) -> {
-			System.err.println("Aplicacion arrancadaaaaaaaaaaaaaa");
-		};
 	}
 
 }
