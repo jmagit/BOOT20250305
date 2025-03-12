4a9162f4e2f3e2d25eba4287bfd39f6a3896dff3
Javier <jmartin@grupoloyal.com>
2025-03-12 15:09:29 +0100

    Limpieza de la consola

--- demo/src/main/java/com/example/DemoApplication.java
+++ demo/src/main/java/com/example/DemoApplication.java
@@ -24,41 +24,46 @@ public class DemoApplication implements CommandLineRunner {
 		
 	}
 	
-//	@Autowired //(required = false)
-//	Servicio srv;
-	
-	@Autowired //(required = false)
-//	@Qualifier("verdad")
-	Repositorio repo1;
-	@Autowired //(required = false)
-//	@Qualifier("mentira")
-	Repositorio repo2;
-//	@Autowired //(required = false)
-//	Repositorio repo;
-
-	@Value("${mi.valor:valor por defecto}")
-	String valor;
-	
-	@Autowired
-	Rango rango;
-	
 	@Override
 	public void run(String... args) throws Exception {
 		System.err.println("Aplicacion arrancada");
-		ejemplosPruebas();
+		ejemplosDatos();
 	}
 	
-	private void ejemplosIOC() {
-		//Servicio srv = new Servicio(new Repositorio(new Configuracion()));
-		//AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
-
-//		srv.guardar();
-//		repo.guardar();
-		repo1.guardar();
-		repo2.guardar();
-		System.err.println("Valor: " + valor);
-		System.err.println("Rango: " + rango);
+	private void ejemplosDatos() {
+		
 	}
+	
+////	@Autowired //(required = false)
+////	Servicio srv;
+//	
+//	@Autowired //(required = false)
+////	@Qualifier("verdad")
+//	Repositorio repo1;
+//	@Autowired //(required = false)
+////	@Qualifier("mentira")
+//	Repositorio repo2;
+////	@Autowired //(required = false)
+////	Repositorio repo;
+//
+//	@Value("${mi.valor:valor por defecto}")
+//	String valor;
+//	
+//	@Autowired
+//	Rango rango;
+//	
+//	private void ejemplosIOC() {
+//		//Servicio srv = new Servicio(new Repositorio(new Configuracion()));
+//		//AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext();
+//
+////		srv.guardar();
+////		repo.guardar();
+//		repo1.guardar();
+//		repo2.guardar();
+//		System.err.println("Valor: " + valor);
+//		System.err.println("Rango: " + rango);
+//	}
+	
 	private void ejemplosPruebas() {
 		var calc = new Calculadora();
 		System.err.println("Suma: " + calc.suma(2, 3));
