801946f59cf70035e6f23d30a9a7204a23a48936
Javier <jmartin@grupoloyal.com>
2025-03-14 13:56:19 +0100

    Proyecciones

--- demo/src/main/java/com/example/domains/entities/dtos/ActorShort.java
+++ demo/src/main/java/com/example/domains/entities/dtos/ActorShort.java
@@ -1,6 +1,10 @@
 package com.example.domains.entities.dtos;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface ActorShort {
-	int getActorId();
+	@Value("#{target.actorId}")
+	int getId();
+	@Value("#{target.lastName + ', ' + target.firstName}")
 	String getNombre();
 }
