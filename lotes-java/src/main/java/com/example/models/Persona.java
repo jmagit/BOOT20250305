801540e0cb29d39a7365eaf9ee332f8b3ec164ae
Javier <jmartin@grupoloyal.com>
2025-03-18 13:39:10 +0100

    CSV a DB

--- lotes-java/src/main/java/com/example/models/Persona.java
+++ lotes-java/src/main/java/com/example/models/Persona.java
@@ -1,8 +1,10 @@
 package com.example.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Data
+@Data @AllArgsConstructor @NoArgsConstructor
 public class Persona {
 	private long id;
 	private String nombre, correo, ip;
