f9fa70fcf9e594bb5bdc24036ef615a45fe57a6c
Javier <jmartin@grupoloyal.com>
2025-03-24 16:23:29 +0100

    Laboratorios SOAP

---
+++ gs-consuming-web-service/target/generated-sources/wsimport/com/example/consumingwebservice/wsdl/Currency.java
@@ -0,0 +1,39 @@
+
+package com.example.consumingwebservice.wsdl;
+
+import jakarta.xml.bind.annotation.XmlEnum;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para currency.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * <pre>
+ * &lt;simpleType name="currency"&gt;
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string"&gt;
+ *     &lt;enumeration value="GBP"/&gt;
+ *     &lt;enumeration value="EUR"/&gt;
+ *     &lt;enumeration value="PLN"/&gt;
+ *   &lt;/restriction&gt;
+ * &lt;/simpleType&gt;
+ * </pre>
+ * 
+ */
+@XmlType(name = "currency")
+@XmlEnum
+public enum Currency {
+
+    GBP,
+    EUR,
+    PLN;
+
+    public String value() {
+        return name();
+    }
+
+    public static Currency fromValue(String v) {
+        return valueOf(v);
+    }
+
+}
