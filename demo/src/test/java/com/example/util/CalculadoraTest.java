3f97adc21ad912b6f3235690b0d32823ed068bba
Javier <35864946+jmagit@users.noreply.github.com>
2025-03-28 14:32:22 +0100

    Display

--- demo/src/test/java/com/example/util/CalculadoraTest.java
+++ demo/src/test/java/com/example/util/CalculadoraTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CalculadoraTest {
@@ -27,12 +28,25 @@ class CalculadoraTest {
 	}
 
 	@Test
+	@DisplayName("Suma dos enteros")
 	void testSuma() {
 		var calc = new Calculadora();
 		
 		var actual = calc.suma(2, 3);
 		
 		assertEquals(5, actual);
+	}
+
+	@Test
+	@DisplayName("Suma dos reales")
+	void testSuma2() {
+		var calc = new Calculadora();
+		
+		var actual = calc.suma(0.1, 0.2);
+
+		assertEquals(0.1, calc.suma(1, -0.9));
+
+		assertEquals(0.3, actual);
 	}
 
 }
