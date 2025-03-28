f1364ff942f5450929cf5e8120dfe59dcef81ee2
Javier <35864946+jmagit@users.noreply.github.com>
2025-03-28 14:32:22 +0100

    excepciones

--- demo/src/main/java/com/example/util/Calculadora.java
+++ demo/src/main/java/com/example/util/Calculadora.java
@@ -1,7 +1,19 @@
 package com.example.util;
 
 public class Calculadora {
+	public int suma(int a, int b) {
+		return a + b;
+	}
 	public double suma(double a, double b) {
 		return a + b;
+	}
+	public int divide(int a, int b) {
+		return a / b;
+	}
+	public double divide(double a, double b) {
+		if(b == 0) {
+			throw new ArithmeticException("/ by zero");
+		}
+		return a / b;
 	}
 }
--- demo/src/test/java/com/example/util/CalculadoraTest.java
+++ demo/src/test/java/com/example/util/CalculadoraTest.java
@@ -36,6 +36,18 @@ class CalculadoraTest {
 		
 		assertEquals(5, actual);
 	}
+	@Test
+	@DisplayName("Suma dos enteros grandes")
+	void testSumaInt() {
+		var calc = new Calculadora();
+		
+		var actual = calc.suma(Integer.MAX_VALUE, 1);
+		assertEquals(5, actual);
+		
+		assertTrue(1.0 / 0 > 0);
+		assertEquals(5, 1.0 / 0);
+//		assertTrue(actual > 0);
+	}
 
 	@Test
 	@DisplayName("Suma dos reales")
@@ -44,9 +56,28 @@ class CalculadoraTest {
 		
 		var actual = calc.suma(0.1, 0.2);
 
-		assertEquals(0.1, calc.suma(1, -0.9));
-
 		assertEquals(0.3, actual);
+	}
+	@Test
+	@DisplayName("Suma dos reales: resta")
+	void testSuma3() {
+		var calc = new Calculadora();
+		
+		assertEquals(0.1, calc.suma(1, -0.9));
+	}
+	@Test
+	@DisplayName("Divide dos enteros")
+	void testDivide() {
+		var calc = new Calculadora();
+		
+		assertEquals(0.5, calc.divide(1.0, 2));
+	}
+	@Test
+	@DisplayName("Divide por cero")
+	void testDivide2() {
+		var calc = new Calculadora();
+		var ex = assertThrows(ArithmeticException.class, () -> calc.divide(1, 0));
+		assertEquals("/ by zero", ex.getMessage());
 	}
 
 }
