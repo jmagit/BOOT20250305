58f5cd5b7a55b172c073d4cc8ee854ed19202827
Javier <35864946+jmagit@users.noreply.github.com>
2025-03-31 11:33:09 +0200

    HomeComponent

--- demo-ng/src/app/app.component.ts
+++ demo-ng/src/app/app.component.ts
@@ -1,22 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoggerService } from '@my/core';
 import { DemosComponent } from './ejemplos/demos/demos.component';
 import { NotificationComponent, NotificationModalComponent } from './main';
+import { NotificationService, NotificationType } from './common-services';
+import { HomeComponent } from "./main/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DemosComponent, NotificationComponent, NotificationModalComponent, ],
+  imports: [RouterOutlet, DemosComponent, NotificationComponent, NotificationModalComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title: string = 'world';
 
   // constructor(out: LoggerService) {
   //   out.error('Es un error')
   //   out.warn('Es un warn')
   //   out.info('Es un info')
   //   out.log('Es un log')
+  // }
+  // constructor(private notify: NotificationService) {}
+
+  // ngOnInit(): void {
+  //   this.notify.add('Aplicación arrancada', NotificationType.info)
   // }
 }
--- demo-ng/src/app/main/index.ts
+++ demo-ng/src/app/main/index.ts
@@ -1,2 +1,3 @@
 export { NotificationComponent } from './notification/notification.component'
 export { NotificationModalComponent } from './notification-modal/notification-modal.component'
+export * from './home/home.component'
