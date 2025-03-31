8f9baa8f8f74b0705e44e97d0af9416c286f8bd7
Javier <35864946+jmagit@users.noreply.github.com>
2025-03-31 12:21:09 +0200

    clase del componente

--- demo-ng/src/app/ejemplos/demos/demos.component.ts
+++ demo-ng/src/app/ejemplos/demos/demos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { Unsubscribable } from 'rxjs';
 import { NotificationService, NotificationType } from 'src/app/common-services';
 
@@ -9,7 +9,52 @@ import { NotificationService, NotificationType } from 'src/app/common-services';
   styleUrl: './demos.component.css'
 })
 export class DemosComponent {
+  private fecha = new Date('2025-03-31');
+  public readonly nombre = signal<string>('mundo')
+  public readonly fontSize = signal<number>(24)
+  public readonly listado = signal([
+    { id: 1, nombre: 'Madrid'},
+    { id: 2, nombre: 'OVIEDO'},
+    { id: 3, nombre: 'barcelona'},
+    { id: 4, nombre: 'ciudad Real'},
+  ])
+  public readonly idProvincia = signal<number>(2)
+
+  public readonly resultado = signal<string>('')
+  public readonly visible = signal<boolean>(true)
+  public readonly invisible = computed<boolean>(() => !this.visible())
+  public readonly estetica = signal({ importante: true, urgente: true, error: false })
+
   constructor(public vm: NotificationService) { }
+
+  public get Fecha(): string { return this.fecha.toISOString(); }
+  public set Fecha(value: string) {
+    this.fecha = new Date(value);
+  }
+
+  saluda() {
+    this.resultado.set(`Hola ${this.nombre()}`);
+  }
+  despide() {
+    this.resultado.set(`Adios ${this.nombre()}`);
+  }
+  di(algo: string) {
+    this.resultado.set(`Dice ${algo}`);
+  }
+  cambia() {
+    this.visible.update(valor => !valor);
+    this.estetica.update(valor => ({ ...valor, importante: !valor.importante }));
+    this.estetica.update(valor => ({ ...valor, error: !valor.error }));
+  }
+
+  add(provincia: string) {
+    const id = this.listado()[this.listado().length - 1].id + 1;
+    this.listado.update(valor => [...valor, { id, nombre: provincia }]);
+    this.idProvincia.set(id);
+  }
+
+  calcula(a: number, b:number) { return a + b; }
+
 
   // private suscriptor: Unsubscribable | undefined;
   private suscriptor?: Unsubscribable;
