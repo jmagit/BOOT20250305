5456cdb55f6e6697b9a93da521a6358dc18a1878
Javier <35864946+jmagit@users.noreply.github.com>
2025-04-01 11:25:34 +0200

    formularios

--- demo-ng/src/app/app.component.ts
+++ demo-ng/src/app/app.component.ts
@@ -4,10 +4,11 @@ import { LoggerService } from '@my/core';
 import { DemosComponent } from './ejemplos/demos/demos.component';
 import { NotificationComponent, NotificationModalComponent } from './main';
 import { NotificationService, NotificationType } from './common-services';
+import { FormulariosComponent } from './ejemplos/formularios/formularios.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DemosComponent, NotificationComponent, /*NotificationModalComponent,*/ ],
+  imports: [RouterOutlet, FormulariosComponent, NotificationComponent, /*NotificationModalComponent,*/ ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
