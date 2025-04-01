16d59d86ed30c9cbad6e16bbaea3d1e0e7ca24c3
Javier <35864946+jmagit@users.noreply.github.com>
2025-04-01 15:06:58 +0200

    ejemplos

--- demo-ng/src/app/main/index.ts
+++ demo-ng/src/app/main/index.ts
@@ -1,3 +1,4 @@
 export { NotificationComponent } from './notification/notification.component'
 export { NotificationModalComponent } from './notification-modal/notification-modal.component'
 export * from './home/home.component'
+export * from './ajax-wait'
