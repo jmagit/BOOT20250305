7ff4b4ef73e09b193517504153846a036d30e93c
Javier <35864946+jmagit@users.noreply.github.com>
2025-04-03 08:26:39 +0200

    Semilla del proyecto

--- sakila/src/app/app.component.ts
+++ sakila/src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoggerService } from '@my/core';
+import { AjaxWaitComponent, NotificationComponent, NotificationModalComponent } from './main';
+import { NotificationService, NotificationType } from './common-services';
+import { HeaderComponent } from "./main/header/header.component";
+import { FooterComponent } from "./main/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NotificationComponent, /*NotificationModalComponent,*/
+    AjaxWaitComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'sakila';
+
+  // constructor(out: LoggerService) {
+  //   out.error('Es un error')
+  //   out.warn('Es un warn')
+  //   out.info('Es un info')
+  //   out.log('Es un log')
+  // }
+  // constructor(private notify: NotificationService) {}
+
+  // ngOnInit(): void {
+  //   this.notify.add('Aplicación arrancada', NotificationType.info)
+  // }
 }
--- sakila/src/environments/environment.development.ts
+++ sakila/src/environments/environment.development.ts
@@ -1,1 +1,8 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  ERROR_LEVEL: 99,
+  apiUrl: 'http://localhost:4321/api/',
+  securityApiURL: 'http://localhost:4321/api/',
+  defaultUsername: 'adm@example.com',
+  defaultPassword: 'P@$$w0rd',
+};
